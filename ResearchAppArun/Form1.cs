b433c4aa85690cb0b0280ffcdeeca2c8a5ed5f91
sailendrakc1 <sailendrakc1@gmail.com>
2020-08-28 03:02:04 +1000

    lot of things

--- ResearchAppArun/Form1.cs
+++ ResearchAppArun/Form1.cs
@@ -16,16 +16,10 @@ namespace ResearchAppArun
     public partial class Form1 : Form
     {
 
-        List<int> timeList = new List<int>();
-        List<int> hrList = new List<int>();
-        List<float> tList = new List<float>();
-        List<int> bpList = new List<int>();
-        List<int> rrList = new List<int>();
-        List<int> spo2List = new List<int>();
-        List<mew> pmewsList = new List<mew>();
-        List<frequency> freqList = new List<frequency>();
+        Dictionary<int, mew> pmewsDict = new Dictionary<int, mew>();
+        Dictionary<int, (int, frequency)> PatternCount = new Dictionary<int, (int, frequency)>();
+        Dictionary<int, BaseInput> baseDict = new Dictionary<int, BaseInput>();
 
-        Dictionary<int, int> PatternCount = new Dictionary<int, int>();
         public int totalWindow;
         public Form1()
         {
@@ -59,12 +53,15 @@ namespace ResearchAppArun
             {
                 var md5 = new MD5CryptoServiceProvider();
                 
+                //calculation of pmews table and frequency table and trend table
                 while (!csvReader.EndOfStream)
                 {
                     string line = csvReader.ReadLine();
                     string[] values = line.Split(',');
                     string pattern = "";
                     int ncount = 0;
+                    int time = 0;
+
                     //make pattern out of string
                     for (int i = 0; i < 6; i++) // coz we have 6 colums in csv
                     {
@@ -78,6 +75,7 @@ namespace ResearchAppArun
                         switch (i)
                         {
                             case 0: //time do nothing for now
+                                time = int.Parse(values[i]);
                                 break;
                             case 1: //HR
                                 int tmphr = int.Parse(values[i]);
@@ -189,72 +187,172 @@ namespace ResearchAppArun
                         }
                     }
 
-                    //calculate for frequency table
                     //calculate patterID
                     int patternid = pattern.GetHashCode();
+                    BaseInput bi = new BaseInput();
+
+                    bi.pattern = pattern;
+                    bi.patternIDid = patternid;
+                    bi.time = time;
+
+                    baseDict.Add(patternid, bi);
 
                     //update pattern count
                     if (PatternCount.ContainsKey(patternid))
                     {
+                        var old = PatternCount[patternid];
+
                         //increase count
-                        PatternCount[patternid] += 1;
+                        int newcount = (old.Item1 + 1);
+
+                        //add new time 
+                        old.Item2.times.Add(time);
+
+                        //update tuple
+                        PatternCount[patternid] = (newcount, old.Item2);
+
+                        //no need to change mew
+                        continue;
                     }
                     else
                     {
+                        //calc freq percentage
+                        double freqper = Math.Round(((double)(PatternCount[patternid].Item1 * 100) / totalWindow), 1, MidpointRounding.ToEven);
+
+                        //make frequency class
+                        frequency f = new frequency();
+                        f.totalWindow = totalWindow;
+                        f.freqPercentage = freqper;
+                        f.patterncount = PatternCount[patternid].Item1;
+                        f.patternno = patternid;
+                        f.pattern = pattern;
+
                         //add to dict
-                        PatternCount.Add(patternid, 1);
-                    }
+                        PatternCount.Add(patternid, (1, f));
 
-                    //calc freq percentage
-                    double freqper = Math.Round(((double)(PatternCount[patternid] * 100) / totalWindow), 1 , MidpointRounding.ToEven);
+                        //calculate the mew for this freq
+                        int mews = 0;
 
-                    //make frequency class
-                    frequency f = new frequency();
-                    f.totalWindow = totalWindow;
-                    f.freqPercentage = freqper;
-                    f.patterncount = PatternCount[patternid];
-                    f.patternno = patternid;
-                    f.pattern = pattern;
-
-                    //add to the list
-                    freqList.Add(f);
-
-                    //calculate for pmews 
-                    int mews = 0;
-
-                    //calculate mew out of pattern
-                    foreach (var item in pattern)
-                    {
-                        if(item == 'N')
+                        //calculate mew out of pattern
+                        foreach (var item in pattern)
                         {
+                            if (item == 'N')
+                            {
+                                continue;
+                            }
+
+                            mews += int.Parse(item.ToString());
+                        }
+
+                        //calculate total trust.
+                        float ttrust = mews + ((5 - ncount) / 5);
+
+                        if(ttrust < 3)
+                        {
+                            // no need
                             continue;
                         }
 
-                        mews += int.Parse(item.ToString());
+                        //create a new mew class
+                        mew m = new mew();
+                        m.mews = mews;
+                        m.pattern = pattern;
+                        m.patternNo = patternid;
+                        m.mewtrust = ttrust;
+
+                        //save it to dict
+                        pmewsDict.Add(patternid, m);
                     }
 
-                    //calculate total trust.
-                    float ttrust = mews + ((5 - ncount) / 5);
+                }
 
-                    if (ttrust < 3) // ignore for pmews if it is lesser than 3
+
+
+                //calculate final trend table
+                foreach (var item in PatternCount.Values) 
+                {
+                    if(item.Item2.times.Count < 2)
                     {
                         continue;
                     }
 
-                    //create a new mew class
-                    mew m = new mew();
-                    m.mews = mews;
-                    m.pattern = pattern;
-                    m.patternNo = patternid;
-                    m.mewtrust = ttrust;
+                    double trend = Math.Round((double)((item.Item2.times.Last() - item.Item2.times.First())/(item.Item2.times.Count-1)), 1, MidpointRounding.ToEven);
+                    item.Item2.trendvalue = trend;
+                }
 
-                    //save it to list
-                    pmewsList.Add(m);
+            }
+
+        }
+
+        public double sloper(int n) //we need range not divide in n parts
+        {
+            Dictionary<(int, int), int> tempdict = new Dictionary<(int,int), int>(); // (pid,n), count
+            double timemean = 0;
+
+            var x = baseDict.Count - (baseDict.Count % n); //portion per n
+            var oldt = 0;
+            var t = n*x; //time
+            if(baseDict.Count - t < x)
+            {
+                t = baseDict.Count;
+            }
+
+            for (int i = 1; i <=n ; i++)
+            {
+                timemean += (timemean + (timemean * i));
+            }
+
+            timemean = timemean / n;
+
+            for (int i = 1; i <= n; i++)
+            {
+                foreach (var item in PatternCount.Values)
+                {
+                    int count = 0;
+                    foreach (var ite in item.Item2.times)
+                    {
+                        if (ite >= oldt && ite < t)
+                        {
+                            count += 1;
+                        }
+                    }
+
+                    if(count > 1)
+                    {
+                        tempdict.Add((item.Item2.patternno,i), count);
+
+                        //calculate the slope here insted of adding
+                        var xminmean = x - timemean;
+                    }
+                }
+
+                oldt = t;
+                n += 1;
+                t = n * x;
+                if (baseDict.Count - t < x)
+                {
+                    t = baseDict.Count;
                 }
             }
 
-            //calculate the step 2 table
+            Dictionary<int, double> finalres = new Dictionary<int, double>(); // id , slope
 
+            for (int i = 1; i <= n; i++)
+            {
+                foreach (var item in tempdict)
+                {
+                    int id = item.Key.Item1;
+
+                    if(finalres.ContainsKey(id))
+                    {
+                        continue;
+                    }
+                    int x1 = 
+
+                }
+            }
+
+            return 1;
         }
 
         private void fileLinkTB_TextChanged(object sender, EventArgs e)
@@ -277,6 +375,19 @@ namespace ResearchAppArun
             public int patterncount;
             public int totalWindow;
             public double freqPercentage;
+
+            //time list for trend calculation
+            public List<int> times = new List<int>();
+
+            //trend value
+            public double trendvalue;
+        }
+
+        public class BaseInput
+        {
+            public string pattern;
+            public int time;
+            public int patternIDid;
         }
 
     }
