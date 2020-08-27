3b7bdf5205206217f41e89fd53ffc18c2c882940
sailendrakc1 <sailendrakc1@gmail.com>
2020-08-27 22:43:08 +1000

    Add project files.

---
+++ ResearchAppArun/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ResearchAppArun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileLinkTB = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.OpenBtn = new System.Windows.Forms.Button();
+            this.msgLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // fileLinkTB
+            // 
+            this.fileLinkTB.Location = new System.Drawing.Point(120, 22);
+            this.fileLinkTB.Name = "fileLinkTB";
+            this.fileLinkTB.ReadOnly = true;
+            this.fileLinkTB.Size = new System.Drawing.Size(356, 20);
+            this.fileLinkTB.TabIndex = 1;
+            this.fileLinkTB.TextChanged += new System.EventHandler(this.fileLinkTB_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // OpenBtn
+            // 
+            this.OpenBtn.Location = new System.Drawing.Point(13, 18);
+            this.OpenBtn.Name = "OpenBtn";
+            this.OpenBtn.Size = new System.Drawing.Size(81, 23);
+            this.OpenBtn.TabIndex = 2;
+            this.OpenBtn.Text = "OpenCSV";
+            this.OpenBtn.UseVisualStyleBackColor = true;
+            this.OpenBtn.Click += new System.EventHandler(this.OpenBtn_Click);
+            // 
+            // msgLbl
+            // 
+            this.msgLbl.AutoSize = true;
+            this.msgLbl.ForeColor = System.Drawing.Color.Red;
+            this.msgLbl.Location = new System.Drawing.Point(32, 325);
+            this.msgLbl.Name = "msgLbl";
+            this.msgLbl.Size = new System.Drawing.Size(51, 13);
+            this.msgLbl.TabIndex = 3;
+            this.msgLbl.Text = "Console: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 547);
+            this.Controls.Add(this.msgLbl);
+            this.Controls.Add(this.OpenBtn);
+            this.Controls.Add(this.fileLinkTB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox fileLinkTB;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button OpenBtn;
+        private System.Windows.Forms.Label msgLbl;
+    }
+}
+
---
+++ ResearchAppArun/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LumenWorks.Framework.IO.Csv;
+
+namespace ResearchAppArun
+{
+    public partial class Form1 : Form
+    {
+        DataTable csvTable = new DataTable();
+
+        List<int> timeList = new List<int>();
+        List<int> hrList = new List<int>();
+        List<float> tList = new List<float>();
+        List<int> bpList = new List<int>();
+        List<int> rrList = new List<int>();
+        List<int> spo2List = new List<int>();
+
+        List<string> testlist = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //to only let the filesystem show csv files to pick
+            openFileDialog1.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenBtn_Click(object sender, EventArgs e)
+        {
+            //select the csv file
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileLinkTB.Text = openFileDialog1.FileName;
+            }
+
+            if (!fileLinkTB.Text.EndsWith(".csv"))
+            {
+                msgLbl.Text = "Console: Please select a csv file.";
+                return;
+            }
+
+            //parse the csv file
+            using (var csvReader = new CsvReader(new StreamReader(File.OpenRead(openFileDialog1.FileName)), true))
+            {
+                csvTable.Load(csvReader);
+
+                //msgLbl.Text = "";
+
+                for (int i = 0; i < csvTable.Rows.Count; i++)
+                {
+                    csvTable.Rows[i]["HR"]
+                }
+                //add items to the list
+                /* foreach (DataRow row in csvTable.Rows)
+                 {
+                     //testlist.Add((string)row["a"]);
+
+                     timeList.Add((int)row["Time"]); // time is int
+                     hrList.Add((int)row["HR"]);
+                     tList.Add((float)row["Temp"]);
+                     bpList.Add((int)row["BP"]);
+                     rrList.Add((int)row["RR"]);
+                     spo2List.Add((int)row["SPO2"]);
+                 }*/
+
+                foreach (DataColumn col in csvTable.Columns)
+                {
+                    testlist.Add(col.ToString());
+                }
+
+                foreach (var item in testlist)
+                {
+                    msgLbl.Text += " " + item + "";
+                }
+            }
+
+            //calculate the step 2 table
+
+        }
+
+        private void fileLinkTB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public class mew
+        {
+            public string pattern;
+            public string patternNo;
+            public int mews;
+        }
+
+    }
+}
